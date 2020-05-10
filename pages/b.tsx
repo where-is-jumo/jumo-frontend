02098693acbe33735f860c92c785acf9e3b9554d
Jake Choi <jake@delightroom.com>
2020-05-10 21:49:33 +0900

    test Pre-rendering and Data Fetching

--- pages/b.tsx
+++ pages/b.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Head from "next/head";
+import {GetStaticProps} from "next";
 
-export default () => (
-    <>
-        <Head>
-            <title>Jumo b</title>
-        </Head>
-        <div>b</div>
-    </>
-)
+interface bProps {
+    testData: {a: string}
+}
+class b extends Component<bProps> {
+    public render() {
+        return (
+            <>
+                <Head>
+                    <title>Jumo b</title>
+                </Head>
+                <div>{this.props.testData.a}</div>
+            </>
+        );
+    }
+}
+
+export const getStaticProps: GetStaticProps = async _ => {
+    const testData = {a:'1'}
+    return {
+        props: {
+            testData
+        }
+    }
+}
+
+export default b
