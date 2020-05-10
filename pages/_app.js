7fa00827794baab5021dbb317c3b3b5fed37d3a1
Jake Choi <jake@delightroom.com>
2020-05-10 21:08:08 +0900

    using scss in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../styles/global.css'
+import '../styles/global.scss'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
